751d4890bea624f7c7c9c425357598072cfe5285
AIA <e>
2021-07-01 20:24:40 +0900

    기본세팅

---
+++ src/test/java/org/zerock/j10/J10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.j10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
